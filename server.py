217db3ba1518a2b7dde3aca1cc25c52f99216202
Adam Carpenter <adamcarpenter8873@gmail.com>
2020-12-09 15:38:26 +0000

    initial file commit

---
+++ server.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
+
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'Something went wrong, your message wasn\'t saved.'
+    else:
+        return 'Something went wrong, try again.'
+
+def write_data(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'Email: {email}, Subject: {subject}, Message: {message}\n')
+
+def write_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
